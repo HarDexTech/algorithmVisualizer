79d97a2419b2c5dc26881b77c8a157cfa0e81cd0
Adesina Ayomide <adesinayomide2@gmail.com>
2025-10-25 23:55:10 +0100

    add initial layout and functionality for algorithm visualizer

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 'use secure'
 
-let char = [1, 5, 9, 7];
+const sectionBar = document.getElementById('sectionBar');
+
+let char = prompt('Enter array:');
+let charArray = JSON.parse(char);
+char = charArray;
+
 for(let i = 0; i < char.length; i++){
-    console.log(char[i]);
+    sectionBar.innerHTML += `
+    <div style="height: ${char[i]}em; width: 70px; background: #aaa;"></div>
+    `
 }
