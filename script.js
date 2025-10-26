c0588985dfdccbdd7dbcb5c03af8671aa6da4233
Adesina Ayomide <adesinayomide2@gmail.com>
2025-10-26 21:02:19 +0100

    refactor: improve HTML structure and enhance JavaScript functionality for sorting visualization

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-'use secure'
+'use strict';
 
 const sectionBar = document.getElementById('sectionBar');
+const bars = document.getElementById('sortBtn');
+const revert = document.getElementById('revertBtn');
 
 let char = prompt('Enter array:');
 let charArray = JSON.parse(char);
+const originalChar = JSON.parse(char);
 char = charArray;
 
-for(let i = 0; i < char.length; i++){
+// let char = [1, 3, 5, 7, 3];
+
+for (let i = 0; i < char.length; i++) {
     sectionBar.innerHTML += `
     <div style="height: ${char[i]}em; width: 70px; background: #aaa;"></div>
     `
+}
+
+const sortBars = () => {
+    sectionBar.innerHTML = ``;
+    char = char.sort();
+    for (let i = 0; i < char.length; i++) {
+        sectionBar.innerHTML += `
+    <div style="height: ${char[i]}em; width: 70px; background: #aaa;"></div>
+    `
+    }
+}
+const revertBars = () => {
+    sectionBar.innerHTML = ``;
+    for (let i = 0; i < originalChar.length; i++) {
+        sectionBar.innerHTML += `
+    <div style="height: ${originalChar[i]}em; width: 70px; background: #aaa;"></div>
+    `
+    }
 }
