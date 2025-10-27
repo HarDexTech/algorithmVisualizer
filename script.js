57814663ddbda72b6d756cd3db6fe8dbbd560ad5
Adesina Ayomide <adesinayomide2@gmail.com>
2025-10-27 08:59:22 +0100

    refactor: clean up commented code and maintain initial character array for sorting

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const sectionBar = document.getElementById('sectionBar');
 const bars = document.getElementById('sortBtn');
 const revert = document.getElementById('revertBtn');
 
-let char = prompt('Enter array:');
-let charArray = JSON.parse(char);
-const originalChar = JSON.parse(char);
-char = charArray;
+// let char = prompt('Enter array:');
+// let charArray = JSON.parse(char);
+// const originalChar = JSON.parse(char);
+// char = charArray;
 
-// let char = [1, 3, 5, 7, 3];
+let char = [1, 3, 5, 7, 3];
 
 for (let i = 0; i < char.length; i++) {
     sectionBar.innerHTML += `
@@ -19,13 +19,22 @@ for (let i = 0; i < char.length; i++) {
 
 const sortBars = () => {
     sectionBar.innerHTML = ``;
-    char = char.sort();
     for (let i = 0; i < char.length; i++) {
-        sectionBar.innerHTML += `
-    <div style="height: ${char[i]}em; width: 70px; background: #aaa;"></div>
-    `
+        while (char[i] > char[i + 1]) {
+            sectionBar.innerHTML = `${char[i] = char[i + 1]}`;
+            sectionBar.innerHTML = `${char[i + 1] = char[i]}`;
+        }
     }
 }
+// const sortBars = () => {
+//     sectionBar.innerHTML = ``;
+//     char = char.sort();
+//     for (let i = 0; i < char.length; i++) {
+//         sectionBar.innerHTML += `
+//     <div style="height: ${char[i]}em; width: 70px; background: #aaa;"></div>
+//     `
+//     }
+// }
 const revertBars = () => {
     sectionBar.innerHTML = ``;
     for (let i = 0; i < originalChar.length; i++) {
