0386e9558be856d2106044da87d44a51b79f240d
Adesina Ayomide <adesinayomide2@gmail.com>
2025-11-04 09:54:29 +0100

    refactor: update sorting logic and clean up commented code in script.js; adjust overflow property in style.css

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 'use strict';
 
-const sectionBar = document.getElementById('sectionBar');
-const bars = document.getElementById('sortBtn');
-const revert = document.getElementById('revertBtn');
+ const sectionBar = document.getElementById('sectionBar');
+ const bars = document.getElementById('sortBtn');
+ const revert = document.getElementById('revertBtn');
 
-// let char = prompt('Enter array:');
-// let charArray = JSON.parse(char);
-// const originalChar = JSON.parse(char);
-// char = charArray;
+ let char = prompt('Enter array:');
+ let charArray = JSON.parse(char);
+ const originalChar = JSON.parse(char);
+ char = charArray;
 
-let char = [1, 3, 5, 7, 3];
+ // let char = [1, 3, 5, 7, 3];
 
-for (let i = 0; i < char.length; i++) {
-    sectionBar.innerHTML += `
-    <div style="height: ${char[i]}em; width: 70px; background: #aaa;"></div>
-    `
-}
+ for (let i = 0; i < char.length; i++) {
+     sectionBar.innerHTML += `
+     <div style="height: ${char[i]}em; width: 70px; background: #aaa;"></div>
+     `
+ }
 
-const sortBars = () => {
-    sectionBar.innerHTML = ``;
-    for (let i = 0; i < char.length; i++) {
-        while (char[i] > char[i + 1]) {
-            sectionBar.innerHTML = `${char[i] = char[i + 1]}`;
-            sectionBar.innerHTML = `${char[i + 1] = char[i]}`;
-        }
-    }
-}
-// const sortBars = () => {
-//     sectionBar.innerHTML = ``;
-//     char = char.sort();
-//     for (let i = 0; i < char.length; i++) {
-//         sectionBar.innerHTML += `
-//     <div style="height: ${char[i]}em; width: 70px; background: #aaa;"></div>
-//     `
-//     }
-// }
-const revertBars = () => {
-    sectionBar.innerHTML = ``;
-    for (let i = 0; i < originalChar.length; i++) {
-        sectionBar.innerHTML += `
-    <div style="height: ${originalChar[i]}em; width: 70px; background: #aaa;"></div>
-    `
-    }
-}
+ const sortBars = () => {
+     sectionBar.innerHTML = ``;
+     for (let i = 0; i < char.length; i++) {
+         for (let j = 0; j < char.length; j++) {
+           if (char[j] > char[j + 1]) {
+                // swap
+                [char[j], char[j + 1]] = [char[j + 1], char[j]];
+            }
+         }
+     }
+
+     for (let i = 0; i < char.length; i++) {
+         sectionBar.innerHTML += `
+     <div style="height: ${char[i]}em; width: 70px; background: #aaa;"></div>
+     `;
+     }
+ }
+
+ const revertBars = () => {
+     sectionBar.innerHTML = ``;
+     for (let i = 0; i < originalChar.length; i++) {
+         sectionBar.innerHTML += `
+     <div style="height: ${originalChar[i]}em; width: 70px; background: #aaa;"></div>
+     `
+     }
+ }
