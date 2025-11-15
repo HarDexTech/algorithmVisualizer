9aa5ac7180dc5857336ec3feb104fccf1cca91fa
Adesina Ayomide <adesinayomide2@gmail.com>
2025-11-15 12:29:06 +0100

    refactor: enhance input handling and update button labels in index.html and script.js; tidy up CSS formatting

--- script.js
+++ script.js
@@ -1,45 +1,35 @@
-'use strict';
+"use strict";
 
- const sectionBar = document.getElementById('sectionBar');
- const bars = document.getElementById('sortBtn');
- const revert = document.getElementById('revertBtn');
+const sectionBar = document.getElementById("sectionBar");
+const bars = document.getElementById("sortBtn");
+const revert = document.getElementById("revertBtn");
+const input = document.getElementById("inputArray");
 
- let char = prompt('Enter array:');
- let charArray = JSON.parse(char);
- const originalChar = JSON.parse(char);
- char = charArray;
+bars.classList.add("invisible");
 
- // let char = [1, 3, 5, 7, 3];
-
- for (let i = 0; i < char.length; i++) {
-     sectionBar.innerHTML += `
-     <div style="height: ${char[i]}em; width: 70px; background: #aaa;"></div>
-     `
- }
-
- const sortBars = () => {
-    // Do the sorting (your current logic)
-    for (let i = 0; i < char.length; i++) {
-        for (let j = 0; j < char.length - 1; j++) {
-            if (char[j] > char[j + 1]) {
-                [char[j], char[j + 1]] = [char[j + 1], char[j]];
-            }
-        }
+const sortBars = () => {
+  let char = JSON.parse(document.querySelector("input").value);
+  // Do the sorting (your current logic)
+  for (let i = 0; i < char.length; i++) {
+    for (let j = 0; j < char.length - 1; j++) {
+      if (char[j] > char[j + 1]) {
+        [char[j], char[j + 1]] = [char[j + 1], char[j]];
+      }
     }
-    
-    setTimeout(() => {
-        sectionBar.innerHTML = ``;
-        for (let i = 0; i < char.length; i++) {
-            sectionBar.innerHTML += `<div style="height: ${char[i]}em; width: 70px; background: #aaa;"></div>`;
-        }
-    }, 2000);
-}
+  }
+  sectionBar.innerHTML = ``;
+  for (let i = 0; i < char.length; i++) {
+    sectionBar.innerHTML += `<div style="height: ${char[i]}em; width: 70px; background: #aaa;"></div>`;
+  }
+};
 
- const revertBars = () => {
-     sectionBar.innerHTML = ``;
-     for (let i = 0; i < originalChar.length; i++) {
-         sectionBar.innerHTML += `
+const revertBars = () => {
+  let originalChar = JSON.parse(document.querySelector("input").value);
+  sectionBar.innerHTML = ``;
+  bars.classList.remove("invisible");
+  for (let i = 0; i < originalChar.length; i++) {
+    sectionBar.innerHTML += `
      <div style="height: ${originalChar[i]}em; width: 70px; background: #aaa;"></div>
-     `
-     }
- }
+     `;
+  }
+};
